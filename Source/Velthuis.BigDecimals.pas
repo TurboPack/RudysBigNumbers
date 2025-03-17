4a1ee74ff4e6c6143e45e5a24edcfff16c5be3b7
TommiPrami <tommi.prami@gmail.com>
2025-03-17 13:33:24 +0200

    Left -parameters as upper case

--- Source/Velthuis.BigDecimals.pas
+++ Source/Velthuis.BigDecimals.pas
@@ -387,7 +387,7 @@ type
     /// <param name="Left">The first operand</param>
     /// <param name="Right">The second operand</param>
     /// <returns><code>Result := Left &lt; Right;</code></returns>
-    class operator LessThan(const left, Right: BigDecimal): Boolean;
+    class operator LessThan(const Left, Right: BigDecimal): Boolean;
 
     /// <summary>Returns True if Left is mathematically greater than or equal to Right.</summary>
     /// <param name="Left">The first operand</param>
@@ -405,7 +405,7 @@ type
     /// <param name="Left">The first operand</param>
     /// <param name="Right">The second operand</param>
     /// <returns><code>Result := Left = Right;</code></returns>
-    class operator Equal(const left, Right: BigDecimal): Boolean;
+    class operator Equal(const Left, Right: BigDecimal): Boolean;
 
     /// <summary>Returns True if Left is mathematically not equal to Right.</summary>
     /// <param name="Left">The first operand</param>
@@ -1981,7 +1981,7 @@ begin
   Result := FValue.IsNegative;
 end;
 
-class operator BigDecimal.LessThan(const left, Right: BigDecimal): Boolean;
+class operator BigDecimal.LessThan(const Left, Right: BigDecimal): Boolean;
 begin
   Result := Compare(Left, Right) < 0;
 end;
@@ -2064,7 +2064,7 @@ end;
 
 class operator BigDecimal.NotEqual(const Left, Right: BigDecimal): Boolean;
 begin
-  Result := Compare(left, Right) <> 0;
+  Result := Compare(Left, Right) <> 0;
 end;
 
 class function BigDecimal.Parse(const S: string; const Settings: TFormatSettings): BigDecimal;

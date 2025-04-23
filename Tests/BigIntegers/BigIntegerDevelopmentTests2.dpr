0b1a58f134f7add6c89956d9e1444293782842d3
Paul McGee <paulmcgee1969@gmail.com>
2025-04-24 02:14:49 +0800

    Add New BigInt Issues Tests

--- Tests/BigIntegers/BigIntegerDevelopmentTests2.dpr
+++ Tests/BigIntegers/BigIntegerDevelopmentTests2.dpr
@@ -2,6 +2,7 @@ program BigIntegerDevelopmentTests2;
 {
 
   Delphi DUnit Test Project
+
   -------------------------
   This project contains the DUnit test framework and the GUI/Console test runners.
   Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
@@ -10,18 +11,32 @@ program BigIntegerDevelopmentTests2;
 
 }
 
+// FastMM4 can slow down testing a lot.
+{$DEFINE USEFASTMM4}
+
 {$IFDEF CONSOLE_TESTRUNNER}
 {$APPTYPE CONSOLE}
 {$ENDIF}
 
+{$WARN UNIT_EXPERIMENTAL OFF}
+{$WARN CONSTRUCTING_ABSTRACT OFF}
+
 uses
+  {$IFDEF USEFASTMM4}
+  {$ENDIF }
   DUnitTestRunner,
+  Velthuis.RandomNumbers in '..\..\Source\Velthuis.RandomNumbers.pas',
+  Velthuis.BigIntegers in '..\..\Source\Velthuis.BigIntegers.pas',
   TestVelthuisBigIntRandom in 'TestVelthuisBigIntRandom.pas',
   U_Random_2 in 'U_Random_2.pas';
 
 {$R *.RES}
 
 begin
+{$IFDEF USEFASTMM4}
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
+  DoDebug := False;
   DUnitTestRunner.RunRegisteredTests;
 end.
 
